4e05e1028920a2bc10fe269a75c88377733eb839
anthonynbui <48116437+anthonynbui@users.noreply.github.com>
2024-03-13 18:32:45 -0700

    Guesses implemented

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,26 +15,30 @@ import {
 } from "firebase/firestore";
 import { db } from "../config/auth";
 import { Autocomplete, TextField } from "@mui/material";
+import mystery_player from './mystery_player.png';
 
 function Card() {
   const [rendered, setRendered] = useState(false);
   const [player, setPlayer] = useState({});
   const [playerName, setPlayerName] = useState("");
   const [playerList, setPlayerList] = useState({});
+  const [score, setScore] = useState(0);
+  const [guesses, setGuesses] = useState(3);
+  const [scoreToAdd, setScoreToAdd] = useState(5);
   const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [hint1Clicked, setHint1Clicked] = useState(false);
+  const [hint2Clicked, setHint2Clicked] = useState(false);
+  const [hint3Clicked, setHint3Clicked] = useState(false);
+  const [hint4Clicked, setHint4Clicked] = useState(false);
+  const [reveal, setReveal] = useState(false);
   const playerCollectionsRef = collection(db, "nba-players");
-  //513 active players
-
-  const handlePlayerChange = (event, value) => {
-    setSelectedPlayer(value);
-  };
 
   function getRandomNumber() {
     // Generate a random number between 0 (inclusive) and 1 (exclusive)
     const randomNumber = Math.random();
 
     // Scale the random number to the range of 1 to 513
-    const scaledNumber = Math.floor(randomNumber * 513) + 1;
+    const scaledNumber = Math.floor(randomNumber * 453) + 1;
 
     return scaledNumber;
   }
@@ -53,57 +57,133 @@ function Card() {
       setPlayer(filteredData[0]);
       setPlayerName(filteredData[0].name);
       setRendered(true);
+      setHint1Clicked(false);
+      setHint2Clicked(false);
+      setHint3Clicked(false);
+      setHint4Clicked(false);
     } catch (err) {
       console.log(err);
     }
   };
 
-  const getPlayers2 = async () => {
-    try {
-      const playerDocs = await getDocs(
-        query(playerCollectionsRef, orderBy("id"), limit(1))
-      );
-      const randomPlayerDoc = playerDocs.docs[0];
-      const randomPlayer = {
-        ...randomPlayerDoc.data(),
-        id: randomPlayerDoc.id,
-      };
-      console.log(randomPlayer);
-      return randomPlayer;
-    } catch (err) {
-      console.log(err);
+  const handleHint1Click = () => {
+    setHint1Clicked(true);
+    decrementScore();
+  }
+
+  const handleHint2Click = () => {
+    setHint2Clicked(true);
+    decrementScore();
+  }
+
+  const handleHint3Click = () => {
+    setHint3Clicked(true);
+    decrementScore();
+  }
+
+  const handleHint4Click = () => {
+    setHint4Clicked(true);
+    decrementScore();
+  }
+
+  const handlePlayerChange = (event, value) => {
+    setSelectedPlayer(value);
+
+    if (value === player.name) {
+      console.log("Correct guess")
+      resetPlayers();
+      addScore(scoreToAdd);
+      setScoreToAdd(5);
+      setGuesses(3);
+    } else {
+      console.log("Incorrect guess")
+      decrementGuess();
     }
   };
 
-  const testPlayers = ["bob", "asf"];
+  const decrementScore = () => {
+    setScoreToAdd(scoreToAdd - 1);
+  };
+
+  const decrementGuess = () => {
+    setGuesses(guesses - 1);
+  };
+
+  const addScore = () => {
+    setScore(score + scoreToAdd);
+  }
+
+  function resetPlayers() {
+    // Set reveal to true to show the player
+    setReveal(true);
+  
+    // After 3 seconds, hide the player, get new player, and reset guesses
+    setTimeout(() => {
+      setReveal(false); // Hide the player
+      getPlayers(); // Get a new player
+      setGuesses(3); // Reset guesses
+    }, 3000);
+  }
+  
 
   useEffect(() => {
     getPlayers();
   }, []);
 
   useEffect(() => {
-    console.log(player);
+    if (guesses === 0) {
+      resetPlayers();
+    }
+  }, [guesses]);
+
+  useEffect(() => {
+    console.log(player.name);
   }, [player]);
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+      <h1>Guess the NBA Player</h1>
       <div>
-      <Autocomplete
+        <h5>Score: {score}</h5>
+        <h5>Guesses: {guesses}</h5>
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <Autocomplete
           disablePortal
           id="nba-player-guess"
           options={getPlayerList()}
-          sx={{ paddingLeft: '40px',width: 300 }}
+          sx={{ paddingLeft: "40px", width: 300 }}
           onChange={handlePlayerChange}
           renderInput={(params) => <TextField {...params} label="NBA Player" />}
         />
-        {rendered ? (
+        {reveal ? (
           <img src={player.image_url} width="400" alt="Player" />
         ) : (
-          <span>?</span>
+          <img src={mystery_player} width="400" alt="MysteryPlayer" />
         )}
       </div>
-              <Button onClick={getPlayers}>Randomize Player</Button>
-
+      {/* <div>
+        <Button variant="contained" onClick={getPlayers}>Randomize Player</Button>
+      </div> */}
+      {rendered ? (
+        <div style={{ marginTop: "10px" }}>
+          <div>
+            {hint1Clicked ? (<Button variant="contained" onClick={handleHint1Click} size="large">Position: {player.position}</Button>) : (<Button variant="contained" onClick={handleHint1Click} size="large">Hint 1</Button>)}
+          </div>
+          <div>
+          {hint2Clicked ? (<Button variant="contained" onClick={handleHint2Click} size="large">PPG: {(parseFloat(player.pts) / parseFloat(player.gp)).toFixed(1)}</Button>
+) : (
+  <Button variant="contained" onClick={handleHint2Click} size="large">Hint 2</Button>
+)}
+          </div>
+          <div>
+            {hint3Clicked ? (<Button variant="contained" onClick={handleHint3Click} size="large">Height: {player.height}</Button>) : (<Button variant="contained" onClick={handleHint3Click} size="large">Hint 3</Button>)}
+          </div>
+          <div>
+            {hint4Clicked ? (<Button variant="contained" onClick={handleHint4Click} size="large">Height: {player.team}</Button>) : (<Button variant="contained" onClick={handleHint4Click} size="large">Hint 4</Button>)}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+function Header(){
+    return(
+        <div class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            GymLog
+          </a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Profile
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="#"
+                  onClick={onSignout}
+                >
+                  Signout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+    
+}
+
+export default Header;
