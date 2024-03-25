7235695ae689cdd1fd9833e362adff747a9cedbe
anthonynbui <48116437+anthonynbui@users.noreply.github.com>
2024-03-24 20:39:01 -0700

    .env + Github secrets

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCI5xTSY6Y1TI4FyvtGFHKyaTevnmf7OVE",
-  authDomain: "guess-nba-player-c79d2.firebaseapp.com",
-  projectId: "guess-nba-player-c79d2",
-  storageBucket: "guess-nba-player-c79d2.appspot.com",
-  messagingSenderId: "684505876685",
-  appId: "1:684505876685:web:0f891cf41a58cc14804184",
-  measurementId: "G-E2TV7EKMF9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
